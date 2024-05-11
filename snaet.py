773b49f1c893737ea9e99d1bc37ddeaf21ab8b82
semen <fgf@mail.ru>
2024-05-11 19:02:45 +0300

    add menu

--- snaet.py
+++ snaet.py
@@ -3,13 +3,22 @@ from kivymd.uix.label import MDLabel
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
 from kivy.core.window import Window
+from kivymd.uix.boxlayout import BoxLayout
 from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivymd.app import MDApp
+from kivy.properties import ObjectProperty
 from kivymd.uix.datatables import MDDataTable
 
 
 Window.size = (520, 900)
+
+
+
+
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
 
 
 class MainApp(MDApp):
@@ -19,23 +28,29 @@ class MainApp(MDApp):
 
 
 
-    def rega(self, b):
+    def rega(self):
 
         self.root.ids.screen_man.current = "screen2"
 
 
-
-        self.root.ids.f2.add_widget(self.a)
-
     def registration(self):
         self.root.ids.screen_man.current = "screen3"
 
+    def back(self, screen):
+        self.root.ids.screen_man.current = screen
+        self.root.nav_drawer.set_state("close")
+    def order1(self):
+        self.root.ids.screen_man.current = "screen4"
 
-
-
+    def order2(self):
+        self.root.ids.screen_man.current = "screen5"
 
     def build(self):
         return Builder.load_file("aboba.kv")
+
+
+
+
 
 
 MainApp().run()
