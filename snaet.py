2b8df44fd65dbcf5befbcf9d5695ae6d091ecf08
semen <fgf@mail.ru>
2024-05-23 19:46:23 +0300

    add menu

--- snaet.py
+++ snaet.py
@@ -7,6 +7,7 @@ from kivymd.uix.boxlayout import BoxLayout
 from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivymd.app import MDApp
+from kivymd.uix.menu import MDDropdownMenu
 from kivy.properties import ObjectProperty
 from kivymd.uix.datatables import MDDataTable
 
@@ -22,11 +23,55 @@ class ContentNavigationDrawer(BoxLayout):
 
 
 class MainApp(MDApp):
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #
+    #     menu_items = [
+    #         {
+    #             "viewclass": "IconListItem",
+    #             "icon": "git",
+    #             "height": dp(56),
+    #             "text": f"Item {i}",
+    #             "on_release": lambda x=f"Item {i}": self.set_item(x),
+    #         } for i in range(5)]
+    #     self.menu = MDDropdownMenu(
+    #         caller=self.screen.ids.field,
+    #         items=menu_items,
+    #         position="bottom",
+    #         width_mult=4,
+    #   )
     def sin_login(self):
 
         self.root.ids.screen_man.current = "screen2"
 
+    # def open_menu_list(self):
+    #     class MainApp(MDApp):
+    #         def __init__(self, **kwargs):
+    #             super().__init__(**kwargs)
+    #
+    #             menu_items = [
+    #                 {
+    #                     "viewclass": "IconListItem",
+    #                     "icon": "git",
+    #                     "height": dp(56),
+    #                     "text": f"Item {i}",
+    #                     "on_release": lambda x=f"Item {i}": self.set_item(x),
+    #                 } for i in range(5)]
+    #             self.menu = MDDropdownMenu(
+    #                 caller=self.screen.ids.field,
+    #                 items=menu_items,
+    #                 position="bottom",
+    #                 width_mult=4,
+    #             )
 
+
+
+
+
+
+
+    def menu_callback(self):
+        pass
 
     def rega(self):
 
@@ -40,6 +85,7 @@ class MainApp(MDApp):
         self.root.ids.screen_man.current = screen
         self.root.nav_drawer.set_state("close")
     def order1(self):
+        self.root.nav_drawer.set_state("close")
         self.root.ids.screen_man.current = "screen4"
 
 
@@ -47,8 +93,29 @@ class MainApp(MDApp):
     def order2(self):
         self.root.ids.screen_man.current = "screen5"
 
+
+    def set(self):
+        self.root.ids.screen_man.current = "screen6"
+
+    def flower(self):
+        self.root.ids.screen_man.current = "screen7"
+
+
+    def set1(self):
+        self.root.ids.screen_man.current = "screen8"
+
+    def flower1(self):
+        self.root.ids.screen_man.current = "screen9"
+
+
+
+
+
+
+
     def build(self):
         return Builder.load_file("aboba.kv")
+
 
 
 
